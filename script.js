c5f57896d7cf892654790dbc5dc17680ef728b6a
marcomuna <marco.muna143@gmail.com>
2026-05-08 15:19:41 +0530

    added lenis

--- script.js
+++ script.js
@@ -54,3 +54,19 @@ function typeEffect() {
 }
 
 typeEffect();
+
+// =============================================================================
+// lenis added
+// =============================================================================
+const lenis = new Lenis({
+    duration: 1.2,
+    smoothWheel: true,
+    smoothTouch: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
