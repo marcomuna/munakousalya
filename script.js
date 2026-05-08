3df7e9a5db8e78c3eea55a0b9edf7db1809910cf
marcomuna <marco.muna143@gmail.com>
2026-05-08 14:45:12 +0530

    Typewritter effect added in home page

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 function showSidebar() {
-      const sidebar = document.querySelector(".sidebar").style.display = "flex"
+  const sidebar = (document.querySelector(".sidebar").style.display = "flex");
+}
+function hideSidebar() {
+  const sidebar = (document.querySelector(".sidebar").style.display = "none");
+}
+
+// =========================================================================
+// Typewrite effect
+// =========================================================================
+const words = [
+  "Software Developer",
+  "Flutter Developer",
+  "3D Designer (Blender)",
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let currentWord = "";
+let isDeleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+  currentWord = words[wordIndex];
+
+  if (!isDeleting) {
+    typing.textContent = currentWord.substring(0, charIndex++);
+  } else {
+    typing.textContent = currentWord.substring(0, charIndex--);
+  }
+
+  let speed = 120;
+
+  if (isDeleting) {
+    speed = 60;
+  }
+
+  if (charIndex === currentWord.length + 1) {
+    isDeleting = true;
+    speed = 1500;
+  }
+
+  if (charIndex === 0) {
+    isDeleting = false;
+    wordIndex++;
+
+    if (wordIndex === words.length) {
+      wordIndex = 0;
     }
-    function hideSidebar() {
-      const sidebar = document.querySelector(".sidebar").style.display = "none"
-    }
+  }
+
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
